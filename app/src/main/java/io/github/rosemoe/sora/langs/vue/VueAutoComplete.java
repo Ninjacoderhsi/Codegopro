708862da49de502bd897ce3d9b4df8d78e2b1fbb
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-12-25 12:53:46 +0330

    Add files via upload

---
+++ app/src/main/java/io/github/rosemoe/sora/langs/vue/VueAutoComplete.java
@@ -0,0 +1,68 @@
+/*
+ *    sora-editor - the awesome code editor for Android
+ *    https://github.com/Rosemoe/CodeEditor
+ *    Copyright (C) 2020-2021  Rosemoe
+ *
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
+ *
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *     USA
+ *
+ *     Please contact Rosemoe by email 2073412493@qq.com if you need
+ *     additional information or have any questions
+ */
+package io.github.rosemoe.sora.langs.vue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.rosemoe.sora.data.CompletionItem;
+import io.github.rosemoe.sora.interfaces.AutoCompleteProvider;
+import io.github.rosemoe.sora.text.TextAnalyzeResult;
+
+/**
+ * Provides auto complete items for HTML Language
+ * This is basic support
+ *
+ * @author Akash Yadav
+ */
+
+public class VueAutoComplete implements AutoCompleteProvider {
+    @Override
+    public List<CompletionItem> getAutoCompleteItems(String prefix, TextAnalyzeResult analyzeResult, int line, int column) {
+        List<CompletionItem> items = new ArrayList<>();
+        for (String tag : VueLanguage.TAGS)
+            if (tag.toLowerCase().startsWith(prefix.toLowerCase()))
+                items.add(tagAsCompletion(tag, "HTML Tag"));
+
+        for (String attr : VueLanguage.ATTRIBUTES)
+            if (attr.toLowerCase().startsWith(prefix.toLowerCase()))
+                items.add(attrAsCompletion(attr, "HTML Attribute"));
+        return items;
+    }
+
+	private CompletionItem attrAsCompletion(String attr, String desc) {
+		final CompletionItem item = new CompletionItem(attr, attr.concat("=\"\""), desc);
+		item.cursorOffset(item.commit.length() - 1);
+		return item;
+	}
+
+	private CompletionItem tagAsCompletion(String tag, String desc) {
+		final String open = "<".concat(tag).concat(">");
+		final String close = "</".concat(tag).concat(">");
+		final CompletionItem item = new CompletionItem(tag, desc);
+		item.commit = open.concat(close);
+		item.cursorOffset(item.commit.length() - close.length());
+		return item;
+	}
+}
