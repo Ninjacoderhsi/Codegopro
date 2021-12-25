7f8679f3c9e9c49a98abbac2c31e611fd99ebc05
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-12-25 13:14:44 +0330

    Create lib.cpp

---
+++ app/src/main/assets/lib.cpp
@@ -0,0 +1,15 @@
+#include <htmlgo.cpp>
+#include <htmlgo.c>
+
+int kain(){
+ if (const new list map){
+  custom main map param define()
+  int h = 11;
+  for ( h + 00 + h++ ) {
+  static h2 ;
+  cout << h2 + z << endl;
+  
+  h++ 
+  }
+  class void static_cast main (){
+  fun ();
